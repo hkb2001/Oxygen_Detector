5473162fc02904ca71813bcfa648fa4c817a9874
hkb2001 <hasnainkbhatti@gmail.com>
2023-05-07 15:14:17 +0500

    home 2.0

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -42,55 +42,123 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Color getColor(double value) {
+      if (value < 60) {
+        return Colors.red;
+      } else if (value < 90) {
+        return Colors.orange;
+      } else {
+        return Colors.green;
+      }
+    }
+
+    String getProgressText(double value) {
+      if (value < 60) {
+        return "Low";
+      } else if (value < 90) {
+        return "Medium";
+      } else {
+        return "High";
+      }
+    }
+
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.white,
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Padding(
-              padding: EdgeInsets.all(40.0),
-              child: Text(
-                "Health",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 40,
-                  color: Colors.white,
-                ),
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Health",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    "Oxygen Saturation",
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  LinearProgressIndicator(
+                    value: _spo2 / 100,
+                    backgroundColor: Colors.grey[300],
+                    valueColor: AlwaysStoppedAnimation<Color>(getColor(_spo2)),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "SPO2: ${_spo2.toStringAsFixed(2)}%",
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        getProgressText(_spo2),
+                        style: TextStyle(
+                          color: getColor(_spo2),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  Text(
+                    "Heart Rate",
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  LinearProgressIndicator(
+                    value: _bpm / 200,
+                    backgroundColor: Colors.grey[300],
+                    valueColor: AlwaysStoppedAnimation<Color>(getColor(_bpm)),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "BPM: ${_bpm.toStringAsFixed(0)}",
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                    ],
+                  ),
+                ],
               ),
             ),
-            const Text(
-              "Oxygen Saturation",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Text(
-              "SPO2: $_spo2",
-              style: const TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Text(
-              "BPM: $_bpm",
-              style: const TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
+            Expanded(
+              child: Container(),
             ),
           ],
         ),
-        bottomNavigationBar: const BottomNavbar(initialIndex: 0),
+        bottomNavigationBar: const BottomNavbar(
+          initialIndex: 0,
+        ),
       ),
     );
   }
